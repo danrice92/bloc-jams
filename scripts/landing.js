f878b8578f7d87192676b16afc2a269b8dc6b9b7
Dan Rice <dan.rice.92@outlook.com>
2016-11-01 17:06:20 -0600

    Checkpoint 23: Events completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,20 +1,13 @@
-var animateTitle = function () {
-    
-    var title = document.getElementsByClassName('hero-title');
-    
-    var revealTitle = function () {
-        title[0].style.opacity = 1;
-        title[0].style.fontSize = "4rem";
-    };
-    
-    revealTitle();
+var title = document.getElementsByClassName('hero-title');
+var points = document.getElementsByClassName('point');
+
+var animateTitle = function(title) {
+    title[0].style.opacity = 1;
+    title[0].style.fontSize = "4rem";
 };
 
-var animatePoints = function () {
-    
-    var points = document.getElementsByClassName('point');
-    
-    var revealPoint = function () {
+var animatePoints = function(points) {
+    var revealPoint = function() {
         for (p = 0; p < points.length; p++) {
             points[p].style.opacity = 1;
             points[p].style.transform = "scaleX(1) translateY(0)";
@@ -22,11 +15,23 @@ var animatePoints = function () {
             points[p].style.WebkitTransform = "scaleX(1) translateY(0)";
         }
     };
-    
     revealPoint();
-    
 };
 
-animateTitle();
-
-animatePoints();
+window.onload = function() {
+    
+    animateTitle(title);
+    
+    if (window.innerHeight > 950) {
+        animatePoints(points);
+    }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(points);
+        }
+    });
+}
