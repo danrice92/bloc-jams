72e8dc6a8ba2e804cf9804090b373c4b621498ec
Dan Rice <dan.rice.92@outlook.com>
2016-11-01 16:35:58 -0600

    Assignment 22: Animation completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,32 @@
-var animatePoints = function() {
+var animateTitle = function () {
+    
+    var title = document.getElementsByClassName('hero-title');
+    
+    var revealTitle = function () {
+        title[0].style.opacity = 1;
+        title[0].style.fontSize = "4rem";
+    };
+    
+    revealTitle();
+};
+
+var animatePoints = function () {
     
     var points = document.getElementsByClassName('point');
-        
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+    
+    var revealPoint = function () {
+        for (p = 0; p < points.length; p++) {
+            points[p].style.opacity = 1;
+            points[p].style.transform = "scaleX(1) translateY(0)";
+            points[p].style.msTransform = "scaleX(1) translateY(0)";
+            points[p].style.WebkitTransform = "scaleX(1) translateY(0)";
+        }
     };
     
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-    
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
+    revealPoint();
     
 };
+
+animateTitle();
+
+animatePoints();
